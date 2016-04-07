b61072139a7aa589a10467a6be2a16f3a40aa6bf
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-04-07 14:47:32 -0400

    fixed unit test import name

--- test/test_class1_binding_predictor.py
+++ test/test_class1_binding_predictor.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-import dummy_predictors
-import dummy_predictors.always_zero_predictor_with_unknown_AAs as predictor
+from dummy_predictors import always_zero_predictor_with_unknown_AAs
 
 
 def test_always_zero_9mer_inputs():
@@ -12,18 +11,18 @@ def test_always_zero_9mer_inputs():
     ]
 
     n_expected = len(test_9mer_peptides)
-    y = predictor.predict_peptides(test_9mer_peptides)
+    y = always_zero_predictor_with_unknown_AAs.predict_peptides(test_9mer_peptides)
     assert len(y) == n_expected
     assert np.all(y == 0)
 
     # call the predict method for 9mers directly
-    y = predictor.predict_9mer_peptides(test_9mer_peptides)
+    y = always_zero_predictor_with_unknown_AAs.predict_9mer_peptides(test_9mer_peptides)
     assert len(y) == n_expected
     assert np.all(y == 0)
 
-    ic50 = predictor.predict_peptides_ic50(test_9mer_peptides)
+    ic50 = always_zero_predictor_with_unknown_AAs.predict_peptides_ic50(test_9mer_peptides)
     assert len(y) == n_expected
-    assert np.all(ic50 == predictor.max_ic50), ic50
+    assert np.all(ic50 == always_zero_predictor_with_unknown_AAs.max_ic50), ic50
 
 
 def test_always_zero_8mer_inputs():
@@ -33,13 +32,13 @@ def test_always_zero_8mer_inputs():
     ]
 
     n_expected = len(test_8mer_peptides)
-    y = predictor.predict_peptides(test_8mer_peptides)
+    y = always_zero_predictor_with_unknown_AAs.predict_peptides(test_8mer_peptides)
     assert len(y) == n_expected
     assert np.all(y == 0)
 
-    ic50 = predictor.predict_peptides_ic50(test_8mer_peptides)
+    ic50 = always_zero_predictor_with_unknown_AAs.predict_peptides_ic50(test_8mer_peptides)
     assert len(y) == n_expected
-    assert np.all(ic50 == predictor.max_ic50), ic50
+    assert np.all(ic50 == always_zero_predictor_with_unknown_AAs.max_ic50), ic50
 
 
 def test_always_zero_10mer_inputs():
@@ -50,21 +49,21 @@ def test_always_zero_10mer_inputs():
     ]
 
     n_expected = len(test_10mer_peptides)
-    y = predictor.predict_peptides(test_10mer_peptides)
+    y = always_zero_predictor_with_unknown_AAs.predict_peptides(test_10mer_peptides)
     assert len(y) == n_expected
     assert np.all(y == 0)
 
-    ic50 = predictor.predict_peptides_ic50(test_10mer_peptides)
+    ic50 = always_zero_predictor_with_unknown_AAs.predict_peptides_ic50(test_10mer_peptides)
     assert len(y) == n_expected
-    assert np.all(ic50 == predictor.max_ic50), ic50
+    assert np.all(ic50 == always_zero_predictor_with_unknown_AAs.max_ic50), ic50
 
 
 def test_encode_peptides_9mer():
-    X = predictor.encode_9mer_peptides(["AAASSSYYY"])
+    X = always_zero_predictor_with_unknown_AAs.encode_9mer_peptides(["AAASSSYYY"])
     assert X.shape[0] == 1, X.shape
     assert X.shape[1] == 9, X.shape
 
-    X, indices = predictor.encode_peptides(["AAASSSYYY"])
+    X, indices = always_zero_predictor_with_unknown_AAs.encode_peptides(["AAASSSYYY"])
     assert len(indices) == 1
     assert indices[0] == 0
     assert X.shape[0] == 1, X.shape
@@ -72,7 +71,7 @@ def test_encode_peptides_9mer():
 
 
 def test_encode_peptides_8mer():
-    X, indices = predictor.encode_peptides(["AAASSSYY"])
+    X, indices = always_zero_predictor_with_unknown_AAs.encode_peptides(["AAASSSYY"])
     assert len(indices) == 9
     assert (indices == 0).all()
     assert X.shape[0] == 9, (X.shape, X)
@@ -80,7 +79,7 @@ def test_encode_peptides_8mer():
 
 
 def test_encode_peptides_10mer():
-    X, indices = predictor.encode_peptides(["AAASSSYYFF"])
+    X, indices = always_zero_predictor_with_unknown_AAs.encode_peptides(["AAASSSYYFF"])
     assert len(indices) == 10
     assert (indices == 0).all()
     assert X.shape[0] == 10, (X.shape, X)
