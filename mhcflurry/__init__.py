cadc5480882080658b51c6a6b7b6efc9bcf23e4e
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-04-19 14:37:38 -0400

    added back some basic imports into __init__

--- mhcflurry/__init__.py
+++ mhcflurry/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .class1_binding_predictor import Class1BindingPredictor
+from .predict import predict
