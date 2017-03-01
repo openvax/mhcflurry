3001021855f8a89f5c455f245c4a49e9df439a54
Tim O'Donnell <timodonnell@gmail.com>
2017-03-01 11:56:29 -0500

    fixes

---
+++ mhcflurry/class1_allele_specific_ensemble/__init__.py
@@ -0,0 +1,8 @@
+from .class1_ensemble_multi_allele_predictor import (
+    Class1EnsembleMultiAllelePredictor,
+    HYPERPARAMETER_DEFAULTS)
+
+__all__ = [
+    "Class1EnsembleMultiAllelePredictor",
+    "HYPERPARAMETER_DEFAULTS",
+]
