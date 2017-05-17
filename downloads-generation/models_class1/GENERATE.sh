503b1b423e0926ea1558c151119082ce98be514c
Tim O'Donnell <timodonnell@gmail.com>
2017-05-17 21:31:54 +0000

    update

--- downloads-generation/models_class1/GENERATE.sh
+++ downloads-generation/models_class1/GENERATE.sh
@@ -31,7 +31,7 @@ time mhcflurry-class1-train-allele-specific-models \
     --data "$(mhcflurry-downloads path data_curated)/curated_training_data.csv.bz2" \
     --hyperparameters hyperparameters.json \
     --out-models-dir models \
-    --min-measurements-per-allele 20
+    --min-measurements-per-allele 100
 
 cp $SCRIPT_ABSOLUTE_PATH .
 tar -cjf "../${DOWNLOAD_NAME}.tar.bz2" *
