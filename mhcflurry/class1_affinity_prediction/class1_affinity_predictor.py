1e93de7677731ae2f14aa4ac1e689d75dddd059f
Rohan Pai <me@rohanpai.com>
2017-06-29 18:11:49 -0400

    fix per tim's comments

--- mhcflurry/class1_affinity_prediction/class1_affinity_predictor.py
+++ mhcflurry/class1_affinity_prediction/class1_affinity_predictor.py
@@ -119,18 +119,17 @@ class Class1AffinityPredictor(object):
             max(lower for (lower, upper) in length_ranges),
             min(upper for (lower, upper) in length_ranges))
 
-    def merge(self, affinity_predictors=[]):
+    def merge(self, affinity_predictors):
         """ 
-        Merge multiple affinity predictors into one.  
+        Merge multiple affinity predictors into current predictor.  
         When training MHCFlurry in parallel, the user might be left with
         multiple affinity predictors, merge can take in multiple affinity predictors
         and the result will be a single ensemble.
         Parameters
         ----------
-        affinity_predictors : array
+        affinity_predictors : list 
             Affinity predictors that want to merge
         """
-        self.allele_to_allele_specific_models
         for affinity_predictor in affinity_predictors:
             for allele in affinity_predictor.allele_to_allele_specific_models.keys():
                 if allele in self.allele_to_allele_specific_models:
