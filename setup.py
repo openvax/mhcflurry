55b6e70c14fb0bb9183601cbb747db54fe36a9f8
Tim O'Donnell <timodonnell@gmail.com>
2017-10-13 11:38:02 -0400

    Bump Keras

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
         'six',
         'numpy>=1.11',
         'pandas>=0.13.1',
-        'Keras==2.0.6',
+        'Keras==2.0.8',
         'appdirs',
         'tensorflow',
         'scikit-learn',
