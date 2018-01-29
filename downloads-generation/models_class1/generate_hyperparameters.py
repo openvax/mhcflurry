6287ee2bf89b54b80c05be6e80f52d8d38214ad2
Tim O'Donnell <timodonnell@gmail.com>
2018-01-28 20:19:11 -0500

    Add robust mean as centrality measure to combine predictions

---
+++ downloads-generation/models_class1/generate_hyperparameters.py
@@ -0,0 +1,76 @@
+"""
+Generate grid of hyperparameters
+"""
+
+from sys import stdout
+from copy import deepcopy
+from yaml import dump
+
+base_hyperparameters = {
+    ##########################################
+    # ENSEMBLE SIZE
+    ##########################################
+    "n_models": 1,
+
+    ##########################################
+    # OPTIMIZATION
+    ##########################################
+    "max_epochs": 500,
+    "patience": 20,
+    "early_stopping": True,
+    "validation_split": 0.1,
+    "minibatch_size": 128,
+    "loss": "custom:mse_with_inequalities",
+
+    ##########################################
+    # RANDOM NEGATIVE PEPTIDES
+    ##########################################
+    "random_negative_rate": 0.2,
+    "random_negative_constant": 25,
+    "random_negative_affinity_min": 20000.0,
+    "random_negative_affinity_max": 50000.0,
+
+    ##########################################
+    # PEPTIDE REPRESENTATION
+    ##########################################
+    # One of "one-hot", "embedding", or "BLOSUM62".
+    "peptide_amino_acid_encoding": "BLOSUM62",
+    "use_embedding": False,  # maintained for backward compatability
+    "embedding_output_dim": 8,  # only used if using embedding
+    "kmer_size": 15,
+
+    ##########################################
+    # NEURAL NETWORK ARCHITECTURE
+    ##########################################
+    "locally_connected_layers": [
+        {
+            "filters": 8,
+            "activation": "tanh",
+            "kernel_size": 3
+        }
+    ],
+    "activation": "relu",
+    "output_activation": "sigmoid",
+    "layer_sizes": [16],
+    "dense_layer_l1_regularization": 0.001,
+    "batch_normalization": False,
+    "dropout_probability": 0.0,
+}
+
+grid = []
+for dense_layer_size in [64, 16]:
+    for num_lc in [0, 1, 2]:
+        for lc_kernel_size in [3, 5]:
+            new = deepcopy(base_hyperparameters)
+            new["layer_sizes"] = [dense_layer_size]
+            (lc_layer,) = new["locally_connected_layers"]
+            lc_layer['kernel_size'] = lc_kernel_size
+            if num_lc == 0:
+                new["locally_connected_layers"] = []
+            elif num_lc == 1:
+                new["locally_connected_layers"] = [lc_layer]
+            elif num_lc == 2:
+                new["locally_connected_layers"] = [lc_layer, deepcopy(lc_layer)]
+                grid.append(new)
+
+dump(grid, stdout)
---
+++ mhcflurry/ensemble_centrality.py
@@ -0,0 +1,37 @@
+"""
+Measures of centrality (e.g. mean) used to combine predictions across an
+ensemble. The input to these functions are log affinities, and they are expected
+to return a centrality measure also in log-space.
+"""
+
+import numpy
+from functools import partial
+
+
+def robust_mean(log_values):
+    """
+    Mean of values falling within the 25-75 percentiles.
+
+    Parameters
+    ----------
+    log_values : 2-d numpy.array
+        Center is computed along the second axis (i.e. per row).
+
+    Returns
+    -------
+    center : numpy.array of length log_values.shape[1]
+
+    """
+    if log_values.shape[1] <= 3:
+        # Too few values to use robust mean.
+        return numpy.nanmean(log_values, axis=1)
+    mask = (
+        (log_values <= numpy.nanpercentile(log_values, 75, axis=1).reshape((-1, 1))) &
+        (log_values >= numpy.nanpercentile(log_values, 25, axis=1).reshape((-1, 1))))
+    return (log_values * mask.astype(float)).sum(1) / mask.sum(1)
+
+CENTRALITY_MEASURES = {
+    "mean": partial(numpy.nanmean, axis=1),
+    "median": partial(numpy.nanmedian, axis=1),
+    "robust_mean": robust_mean,
+}
