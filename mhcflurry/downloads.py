793d8cb5824c94f1ce4de3772f330aff9fb3d383
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-04-08 13:24:40 -0500

    Add Loader to yaml.load call
    This fixes the `calling yaml.load() without Loader=... is deprecated, as
    the default Loader is unsafe` warning messages.
    
    See https://msg.pyyaml.org/load.

--- mhcflurry/downloads.py
+++ mhcflurry/downloads.py
@@ -50,7 +50,7 @@ def get_downloads_metadata():
     """
     global _METADATA
     if _METADATA is None:
-        _METADATA = yaml.load(resource_string(__name__, "downloads.yml"))
+        _METADATA = yaml.load(resource_string(__name__, "downloads.yml"), Loader=yaml.FullLoader)
     return _METADATA
 
 
