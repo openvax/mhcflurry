1a3f68f764102155f8b07b11c4c6680eaebc48f5
Tim O'Donnell <timodonnell@gmail.com>
2019-06-22 16:50:21 -0400

    fix

--- downloads-generation/models_class1_pan/generate_hyperparameters.py
+++ downloads-generation/models_class1_pan/generate_hyperparameters.py
@@ -48,9 +48,10 @@ base_hyperparameters = {
 }
 
 grid = []
-for layer_sizes in [[512, 256], [512, 512],]:
-    for pretrain in [True, False]:
-        for l1 in [0.0, 0.00001, 0.0001, 0.001]:
+for layer_sizes in [[512, 256], [512, 512], [1024, 512]]:
+    for pretrain in [True]:
+        l1_base = 0.000001
+        for l1 in [l1_base, l1_base / 10, l1_base / 100, l1_base / 1000, 0.0]:
             new = deepcopy(base_hyperparameters)
             new["layer_sizes"] = layer_sizes
             new["dense_layer_l1_regularization"] = l1
