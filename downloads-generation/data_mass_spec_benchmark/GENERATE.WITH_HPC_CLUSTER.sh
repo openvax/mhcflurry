3c0fec52413fa6401c78b8888c4561bc1c9d8916
Tim O'Donnell <timodonnell@gmail.com>
2019-10-01 17:40:07 -0400

    fix

--- downloads-generation/data_mass_spec_benchmark/GENERATE.WITH_HPC_CLUSTER.sh
+++ downloads-generation/data_mass_spec_benchmark/GENERATE.WITH_HPC_CLUSTER.sh
@@ -32,15 +32,15 @@ cp $SCRIPT_DIR/write_allele_list.py .
 PEPTIDES=$(mhcflurry-downloads path data_mass_spec_annotated)/annotated_ms.csv.bz2
 REFERENCES_DIR=$(mhcflurry-downloads path data_references)
 
+python write_allele_list.py "$PEPTIDES" --out alleles.txt
+mkdir predictions
+
+# First just chr1 peptides
 python write_proteome_peptides.py \
     "$PEPTIDES" \
     "${REFERENCES_DIR}/uniprot_proteins.csv.bz2" \
     --chromosome 1 \
     --out proteome_peptides.chr1.csv
-
-python write_allele_list.py "$PEPTIDES" --out alleles.txt
-
-mkdir predictions
 
 for kind in with_mass_spec no_mass_spec
 do
@@ -60,7 +60,33 @@ do
         --cluster-script-prefix-path $SCRIPT_DIR/cluster_submit_script_header.mssm_hpc.lsf
 done
 
+# Now all peptides
+python write_proteome_peptides.py \
+    "$PEPTIDES" \
+    "${REFERENCES_DIR}/uniprot_proteins.csv.bz2" \
+    --out proteome_peptides.all.csv
+
+for kind in with_mass_spec no_mass_spec
+do
+    python run_mhcflurry.py \
+        proteome_peptides.all.csv \
+        --chunk-size 500000 \
+        --batch-size 65536 \
+        --models-dir "$(mhcflurry-downloads path models_class1_pan)/models.$kind" \
+        --allele $(cat alleles.txt) \
+        --out "predictions/all.mhcflurry.$kind" \
+        --verbosity 1 \
+        --worker-log-dir "$SCRATCH_DIR/$DOWNLOAD_NAME" \
+        --cluster-parallelism \
+        --cluster-max-retries 15 \
+        --cluster-submit-command bsub \
+        --cluster-results-workdir ~/mhcflurry-scratch \
+        --cluster-script-prefix-path $SCRIPT_DIR/cluster_submit_script_header.mssm_hpc.lsf
+done
+
+
 bzip2 proteome_peptides.chr1.csv
+bzip2 proteome_peptides.all.csv
 
 cp $SCRIPT_ABSOLUTE_PATH .
 bzip2 LOG.txt
