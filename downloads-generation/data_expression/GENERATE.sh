a55170fe7f96dac54c11dde55465fed3dc0acf23
Tim O'Donnell <timodonnell@gmail.com>
2019-10-10 12:15:00 -0400

    update data_expression

--- downloads-generation/data_expression/GENERATE.sh
+++ downloads-generation/data_expression/GENERATE.sh
@@ -28,27 +28,68 @@ cd $SCRATCH_DIR/$DOWNLOAD_NAME
 # Available from SRA [access required] at:
 # https://www.ebi.ac.uk/ega/studies/EGAS00001000610
 
-# CCLE cell lines
-DATASET=ccle
+# CCLE as processed by expression atlas
+DATASET=expression-atlas-22460905
 mkdir $DATASET
 cd $DATASET
-wget -q https://data.broadinstitute.org/ccle/CCLE_RNAseq_rsem_genes_tpm_20180929.txt.gz
-wget -q https://data.broadinstitute.org/ccle/CCLE_miRNA_20181103.gct
+#wget -q https://www.ebi.ac.uk/gxa/experiments-content/E-MTAB-2770/resources/ExperimentDownloadSupplier.RnaSeqBaseline/tpms.tsv
+ls -lh .
+cd ..
+
+# Human protein atlas
+DATASET=human-protein-atlas
+mkdir $DATASET
+cd $DATASET
+wget -q https://www.proteinatlas.org/download/rna_celline.tsv.zip
+wget -q https://www.proteinatlas.org/download/rna_blood_cell_schmiedel.tsv.zip
+wget -q https://www.proteinatlas.org/download/rna_blood_cell_sample_tpm_m.tsv.zip
+wget -q https://www.proteinatlas.org/download/rna_tissue_gtex.tsv.zip
+for i in $(ls *.zip)
+do
+    unzip $i
+    rm $i
+done
+ls -lh .
+cd ..
+
+#DATASET=HEK293
+#mkdir $DATASET
+#cd $DATASET
+#wget -q  "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE85161&format=file" -O GSE85161_RAW.tar
+#tar -xvf GSE85161_RAW.tar
+#rm GSE85161_RAW.tar
+#ls -lh .
+#cd ..
+
+# CCLE cell lines
+# Other source of CCLE information
+#DATASET=ccle
+#mkdir $DATASET
+#cd $DATASET
+#wget -q https://data.broadinstitute.org/ccle/CCLE_RNAseq_rsem_genes_tpm_20180929.txt.gz
+#wget -q https://data.broadinstitute.org/ccle/CCLE_miRNA_20181103.gct
+#cd ..
+
+# PBMC
+DATASET=pbmc
+mkdir $DATASET
+cd $DATASET
+wget -q ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE107nnn/GSE107011/suppl/GSE107011_Processed_data_TPM.txt.gz
 cd ..
 
 # B721.221
-DATASET=b721221
-mkdir $DATASET
-cd $DATASET
-wget -q https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE93315&format=file
-cd ..
+#DATASET=b721221
+#mkdir $DATASET
+#cd $DATASET
+#wget -q https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE93315&format=file
+#cd ..
 
-DATASET=pancan-xena
-mkdir $DATASET
-cd $DATASET
-wget -q https://pancanatlas.xenahubs.net/download/probeMap/hugo_gencode_good_hg19_V24lift37_probemap
-wget -q https://pancanatlas.xenahubs.net/download/EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz
-cd ..
+#DATASET=pancan-xena
+#mkdir $DATASET
+#cd $DATASET
+#wget -q https://pancanatlas.xenahubs.net/download/probeMap/hugo_gencode_good_hg19_V24lift37_probemap
+#wget -q https://pancanatlas.xenahubs.net/download/EB++AdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.xena.gz
+#cd ..
 
 cp $SCRIPT_ABSOLUTE_PATH .
 bzip2 LOG.txt
