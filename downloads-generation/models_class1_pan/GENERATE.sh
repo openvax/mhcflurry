14d096557a5bd42d2c103d7834e6eec7acd402bc
Tim O'Donnell <timodonnell@gmail.com>
2020-01-01 18:02:06 -0500

    refactoring

--- downloads-generation/models_class1_pan/GENERATE.sh
+++ downloads-generation/models_class1_pan/GENERATE.sh
@@ -10,7 +10,7 @@
 set -e
 set -x
 
-DOWNLOAD_NAME=models_class1_pan_unselected
+DOWNLOAD_NAME=models_class1_pan
 SCRATCH_DIR=${TMPDIR-/tmp}/mhcflurry-downloads-generation
 SCRIPT_ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 SCRIPT_DIR=$(dirname "$SCRIPT_ABSOLUTE_PATH")
