19c3ddf3eebc82bab3d1233726ce132e5077b205
Tim O'Donnell <timodonnell@gmail.com>
2020-01-08 09:01:42 -0500

    fix

--- downloads-generation/models_class1_pan_variants/GENERATE.sh
+++ downloads-generation/models_class1_pan_variants/GENERATE.sh
@@ -70,8 +70,8 @@ then
 fi
 
 #VARIANTS=( no_additional_ms_ms_only_0nm ms_only_0nm no_additional_ms_0nm 0nm no_additional_ms no_pretrain compact_peptide 34mer_sequence single_hidden_no_pretrain affinity_only )
-#VARIANTS=( no_additional_ms_ms_only_0nm ms_only_0nm no_additional_ms_0nm 0nm no_additional_ms no_pretrain compact_peptide 34mer_sequence )
- VARIANTS=( no_additional_ms_ms_only_0nm ms_only_0nm no_additional_ms no_pretrain compact_peptide 34mer_sequence )
+ VARIANTS=( no_additional_ms_ms_only_0nm ms_only_0nm no_additional_ms_0nm 0nm no_additional_ms no_pretrain compact_peptide 34mer_sequence )
+#VARIANTS=( no_additional_ms_ms_only_0nm ms_only_0nm no_additional_ms no_pretrain compact_peptide 34mer_sequence )
 
 
 
