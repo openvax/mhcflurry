e7902745edbea6e8cb35b261aec69196075fef4b
Walid Ahmad <walid@perceival.tech>
2023-10-07 21:35:17 -0400

    bump tf

--- setup.py
+++ setup.py
@@ -17,40 +17,35 @@ from setuptools import setup
 
 
 readme_dir = os.path.dirname(__file__)
-readme_filename = os.path.join(readme_dir, 'README.md')
+readme_filename = os.path.join(readme_dir, "README.md")
 
 try:
-    with open(readme_filename, 'r') as f:
+    with open(readme_filename, "r") as f:
         readme = f.read()
 except:
     logging.warning("Failed to load %s" % readme_filename)
     readme = ""
 
 
-with open('mhcflurry/version.py', 'r') as f:
+with open("mhcflurry/version.py", "r") as f:
     version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        f.read(),
-        re.MULTILINE).group(1)
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+    ).group(1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     required_packages = [
-        'six',
-        'pandas>=0.20.3',
-        'appdirs',
-        'scikit-learn',
-        'mhcgnomes>=0.8.4',
-        'pyyaml',
-        'tqdm',
-
-        # Note: tensorflow dependency is commented out here because there are
-        # multiple packages that can provide tensorflow (e.g. tensorflow-macos)
-        # and I'm not sure how to specify a "this or that" dependency.
-        # 'tensorflow>=2.2.0',
+        "six",
+        "pandas>=0.20.3",
+        "appdirs",
+        "scikit-learn",
+        "mhcgnomes>=0.8.4",
+        "pyyaml",
+        "tqdm",
+        "tensorflow>=2.12.0",
     ]
 
     setup(
-        name='mhcflurry',
+        name="mhcflurry",
         version=version,
         description="MHC Binding Predictor",
         author="Tim O'Donnell and Alex Rubinsteyn",
@@ -58,46 +53,46 @@ if __name__ == '__main__':
         url="https://github.com/openvax/mhcflurry",
         license="http://www.apache.org/licenses/LICENSE-2.0.html",
         entry_points={
-            'console_scripts': [
-                'mhcflurry-downloads = mhcflurry.downloads_command:run',
-                'mhcflurry-predict = mhcflurry.predict_command:run',
-                'mhcflurry-predict-scan = mhcflurry.predict_scan_command:run',
-                'mhcflurry-class1-train-allele-specific-models = '
-                    'mhcflurry.train_allele_specific_models_command:run',
-                'mhcflurry-class1-train-pan-allele-models = '
-                    'mhcflurry.train_pan_allele_models_command:run',
-                'mhcflurry-class1-train-processing-models = '
-                    'mhcflurry.train_processing_models_command:run',
-                'mhcflurry-class1-select-allele-specific-models = '
-                    'mhcflurry.select_allele_specific_models_command:run',
-                'mhcflurry-class1-select-pan-allele-models = '
-                    'mhcflurry.select_pan_allele_models_command:run',
-                'mhcflurry-class1-select-processing-models = '
-                    'mhcflurry.select_processing_models_command:run',
-                'mhcflurry-calibrate-percentile-ranks = '
-                    'mhcflurry.calibrate_percentile_ranks_command:run',
-                'mhcflurry-class1-train-presentation-models = '
-                    'mhcflurry.train_presentation_models_command:run',
-                '_mhcflurry-cluster-worker-entry-point = '
-                    'mhcflurry.cluster_parallelism:worker_entry_point',
+            "console_scripts": [
+                "mhcflurry-downloads = mhcflurry.downloads_command:run",
+                "mhcflurry-predict = mhcflurry.predict_command:run",
+                "mhcflurry-predict-scan = mhcflurry.predict_scan_command:run",
+                "mhcflurry-class1-train-allele-specific-models = "
+                "mhcflurry.train_allele_specific_models_command:run",
+                "mhcflurry-class1-train-pan-allele-models = "
+                "mhcflurry.train_pan_allele_models_command:run",
+                "mhcflurry-class1-train-processing-models = "
+                "mhcflurry.train_processing_models_command:run",
+                "mhcflurry-class1-select-allele-specific-models = "
+                "mhcflurry.select_allele_specific_models_command:run",
+                "mhcflurry-class1-select-pan-allele-models = "
+                "mhcflurry.select_pan_allele_models_command:run",
+                "mhcflurry-class1-select-processing-models = "
+                "mhcflurry.select_processing_models_command:run",
+                "mhcflurry-calibrate-percentile-ranks = "
+                "mhcflurry.calibrate_percentile_ranks_command:run",
+                "mhcflurry-class1-train-presentation-models = "
+                "mhcflurry.train_presentation_models_command:run",
+                "_mhcflurry-cluster-worker-entry-point = "
+                "mhcflurry.cluster_parallelism:worker_entry_point",
             ]
         },
         classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'Operating System :: OS Independent',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Operating System :: OS Independent",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Topic :: Scientific/Engineering :: Bio-Informatics",
         ],
         package_data={
-            'mhcflurry': ['downloads.yml'],
+            "mhcflurry": ["downloads.yml"],
         },
         install_requires=required_packages,
         long_description=readme,
         long_description_content_type="text/markdown",
         packages=[
-            'mhcflurry',
+            "mhcflurry",
         ],
     )
